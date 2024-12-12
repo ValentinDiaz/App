2d15633bcfb14841aa4f1162d01b42214e951044
Valentin <valenfacu316@gmail.com>
2024-12-12 15:03:34 -0300

    agrego verificacion por email

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,21 +5,71 @@ import {
   signInWithEmailAndPassword,
   signOut,
   signInWithPopup,
-  GoogleAuthProvider
+  GoogleAuthProvider,
+  sendEmailVerification
 } from '@angular/fire/auth';
 import { doc, Firestore, getDoc } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class authService {
-  constructor(private auth: Auth,private firestore: Firestore) {}
-  register({ email, password }: any) {
-    return createUserWithEmailAndPassword(this.auth, email, password);
+  constructor(private auth: Auth,private firestore: Firestore,    private router: Router
+  ) {
+
   }
-  login({ email, password }: any) {
-    return signInWithEmailAndPassword(this.auth, email, password);
+  
+  async register({ email, password }: any) {
+    try {
+      // Crear usuario
+      const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
+  
+      // Enviar correo de verificación
+      if (userCredential.user) {
+        await sendEmailVerification(userCredential.user);
+        console.log('Correo de verificación enviado');
+      }
+  
+      // Cerrar sesión inmediatamente después de registrar
+      await signOut(this.auth);
+  
+      console.log('Usuario cerrado sesión inmediatamente después de registrar.');
+  
+      return userCredential;
+    } catch (error) {
+      console.error('Error durante el registro:', error);
+      throw error;
+    }
   }
+  
+
+  async login({ email, password }: any) {
+    try {
+      const userCredential = await signInWithEmailAndPassword(this.auth, email, password);
+  
+      if (userCredential.user) {
+        if (!userCredential.user.emailVerified) {
+          throw new Error('Por favor verifica tu email antes de iniciar sesión.');
+        }
+      }
+  
+      return userCredential;
+    } catch (error: unknown) {
+      console.error('Error al iniciar sesión:', error);
+  
+      // Verificar si el error tiene la propiedad 'message' de tipo string
+      if (typeof error === 'object' && error !== null && 'message' in error) {
+        const errorMessage = (error as { message: string }).message;
+        if (errorMessage === 'Por favor verifica tu email antes de iniciar sesión.') {
+          this.router.navigate(['/login']);
+        }
+      }
+  
+      throw error;  // Re-lanzar el error para manejo posterior
+    }
+  }
+
   loginWithGoogle() {
     return signInWithPopup(this.auth, new GoogleAuthProvider());
   }
@@ -27,6 +77,7 @@ export class authService {
   logOut() {
     return signOut(this.auth);
   }
+
   async isAdmin(): Promise<boolean> {
     const currentUser = this.auth.currentUser;
 
